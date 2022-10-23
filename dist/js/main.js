5d3e7cf780d19ab4db3724fde7790bf04e131e66
Max Kravchenko <maks.cross@gmail.com>
2022-10-23 13:07:04 +0300

    Initial dist subtree commit

---
+++ dist/js/main.js
@@ -0,0 +1,1 @@
+"use strict";console.log("Testing JS import file one.");var array1=["a","b","c"];array1.forEach(function(o){return console.log(o)});var matches=document.querySelectorAll("div");console.log(matches),console.log("Testing JS import file two.");
